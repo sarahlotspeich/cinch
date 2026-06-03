6c5552372e78b8d0bc768b4034be4e63ec67d6b7
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-06-03 10:48:22 -0400

    Commit simulation results

---
+++ simulations/vary_error_depend.R
@@ -0,0 +1,87 @@
+# Load packages
+## RUN ONCE: devtools::install_github("sarahlotspeich/mochi")
+library(mochi) ## for moment-based correction
+## RUN ONCE: devtools::install_github("sarahlotspeich/auditDesignR")
+library(auditDesignR) ## for validation study designs 
+
+# Source script for simulate_data() function
+#devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/mochi/refs/heads/main/simulations/sim_data.R")
+source("~/Documents/cinch/simulations/sim_data.R")
+
+# Set useful constants 
+sim_seed = 11422 ## For reproducibility, seed to start each setting
+num_rep = 1000 ## Number of replications per setting
+
+# Set constants that are not varied between simulation settings 
+N = 1000 ## total sample size
+pv = 0.1 ## validation study size
+sigma2U = 0.5 ## error variance (less error-prone group)
+
+# Loop over different error variances and concentration indices 
+df_res = data.frame( ## initialize empty dataframe to hold results
+  true_ci = NA, 
+  less_error_var = NA,
+  more_error_var = NA,
+  gs_ci = NA, 
+  se_gs_ci = NA, 
+  mb_ci = NA, 
+  se_mb_ci = NA, 
+  nv_ci = NA, 
+  se_nv_ci = NA
+)  
+for (ci in c(-0.5, 0, 0.5)) {
+  for (times_more_error in c(1.1, 1.5, 2)) {
+    set.seed(sim_seed) ## for reproducibility 
+    for (r in 1:num_rep) {
+      ## Simulate data for group 1 (less error-prone)
+      dat1 = sim_data(error_sd = sqrt(sigma2U), 
+                      n = N / 2, 
+                      approx_ci = ci, 
+                      pv = pv, 
+                      design = "SRS")
+      
+      ## Simulate data for group 2 (more error-prone)
+      dat2 = sim_data(error_sd = sqrt(sigma2U * times_more_error), 
+                      n = N / 2, 
+                      approx_ci = ci, 
+                      pv = pv, 
+                      design = "SRS")
+      
+      ## Stack the two groups' data 
+      dat = rbind(dat1, dat2)
+      
+      ## Calculate oracle/fully validated CI
+      mu_hat <- mean(dat$Y)
+      varR <- var(dat$R)
+      fit_ci_oracle <- lm(Y ~ R, data = dat)
+      beta1_hat <- fit_ci_oracle$coefficients[2] 
+      oracle_ci <- 2 * varR / mu_hat * beta1_hat 
+      se_oracle_ci <- delta_method_se(
+        outcome = dat$Y, 
+        exposure = dat$X)
+      
+      ## Calculate moment-based and naive CI using mochi()
+      mochi_res <- mochi(outcome = dat$Y, 
+                         unval_exposure = dat$Xstar, 
+                         val_exposure = dat$Xval, 
+                         include_se = TRUE, 
+                         return_naive = TRUE)
+      
+      ## Combine, row stack, and save 
+      df_res = data.frame(
+        true_ci = ci, 
+        less_error_var = sigma2U,
+        more_error_var = sigma2U * times_more_error,
+        gs_ci = oracle_ci, 
+        se_gs_ci = se_oracle_ci, 
+        mb_ci = mochi_res$ci_moment, 
+        se_mb_ci = mochi_res$se_ci_moment, 
+        nv_ci = mochi_res$ci_naive, 
+        se_nv_ci = mochi_res$se_ci_naive) |> 
+        dplyr::bind_rows(df_res)
+      df_res |> 
+        write.csv(file = paste0("simulations/vary_error_depend.csv"), 
+                row.names = FALSE)  
+    }
+  }
+}
---
+++ simulations/vary_error_variance.R
@@ -0,0 +1,74 @@
+# Load packages
+## RUN ONCE: devtools::install_github("sarahlotspeich/mochi")
+library(mochi) ## for moment-based correction
+## RUN ONCE: devtools::install_github("sarahlotspeich/auditDesignR")
+library(auditDesignR) ## for validation study designs 
+
+# Source script for simulate_data() function
+#devtools::source_url("https://raw.githubusercontent.com/sarahlotspeich/mochi/refs/heads/main/simulations/sim_data.R")
+source("~/Documents/cinch/simulations/sim_data.R")
+
+# Set useful constants 
+sim_seed = 11422 ## For reproducibility, seed to start each setting
+num_rep = 1000 ## Number of replications per setting
+
+# Set constants that are not varied between simulation settings 
+N = 1000 ## total sample size
+pv = 0.1 ## validation study size
+
+# Loop over different error variances and concentration indices 
+df_res = data.frame( ## initialize empty dataframe to hold results
+  true_ci = NA, 
+  error_var = NA,
+  gs_ci = NA, 
+  se_gs_ci = NA, 
+  mb_ci = NA, 
+  se_mb_ci = NA, 
+  nv_ci = NA, 
+  se_nv_ci = NA
+) 
+for (ci in c(-0.5, 0, 0.5)) {
+  for (sigma2U in c(0.1, 0.5, 1)) {
+    set.seed(sim_seed) ## for reproducibility 
+    for (r in 1:num_rep) {
+      ## Simulate data 
+      dat = sim_data(error_sd = sqrt(sigma2U), 
+                     n = 1000, 
+                     approx_ci = ci, 
+                     pv = 0.1, 
+                     design = "SRS")
+      
+      ## Calculate oracle/fully validated CI
+      mu_hat <- mean(dat$Y)
+      varR <- var(dat$R)
+      fit_ci_oracle <- lm(Y ~ R, data = dat)
+      beta1_hat <- fit_ci_oracle$coefficients[2] 
+      oracle_ci <- 2 * varR / mu_hat * beta1_hat 
+      se_oracle_ci <- delta_method_se(
+        outcome = dat$Y, 
+        exposure = dat$X)
+      
+      ## Calculate moment-based and naive CI using mochi()
+      mochi_res <- mochi(outcome = dat$Y, 
+                         unval_exposure = dat$Xstar, 
+                         val_exposure = dat$Xval, 
+                         include_se = TRUE, 
+                         return_naive = TRUE)
+      
+      ## Combine, row stack, and save 
+      df_res = data.frame(
+        true_ci = ci, 
+        error_var = sigma2U,
+        gs_ci = oracle_ci, 
+        se_gs_ci = se_oracle_ci, 
+        mb_ci = mochi_res$ci_moment, 
+        se_mb_ci = mochi_res$se_ci_moment, 
+        nv_ci = mochi_res$ci_naive, 
+        se_nv_ci = mochi_res$se_ci_naive) |> 
+        dplyr::bind_rows(df_res)
+      df_res |> 
+        write.csv(file = paste0("simulations/vary_error_variance.csv"), 
+                row.names = FALSE)  
+    }
+  }
+}
