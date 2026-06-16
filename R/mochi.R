8a41ee3e2bc9ec42dd91b2092d1275c5f3749ce5
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-06-16 15:37:32 -0400

    Add bootstrap capability and quantile-based confidence intervals

--- R/mochi.R
+++ R/mochi.R
@@ -5,16 +5,20 @@
 #' @param unval_exposure vector (of the same length as \code{outcome}) containing the error-prone, unvalidated exposure on which all observations will be ranked
 #' @param val_exposure vector (of the same length as \code{outcome}) containing the error-free, validated exposure on which all observations will be ranked. For observations that were not validated, this vector should contain \code{NA}.
 #' @param return_naive logical for whether the naive estimate (based only on \code{unval_exposure}) should be returned (if \code{TRUE}). The default is \code{FALSE}, in which case only the moment-based estimate is returned.
-#' @param include_se logical for whether jackknife standard error estimated should be returned
+#' @param include_se logical for whether standard error estimate should be returned. The default is \code{FALSE}.
+#' @param bootstraps scalar for how many bootstrap resamples should be used to estimate standard errors (if \code{include_se = TRUE}). The default is \code{1000}. For jackknife (leave-one-out) resampling, let \code{bootstraps = 0}.
+#' @param conf_level scalar (between 0 and 1) for confidence level of confidence intervals (if \code{include_se = TRUE}). The default is \code{0.95} for 95% confidence intervals. 
 #' @param rank_ascend logical for whether exposure is ranked in ascending or descending order. The default is \code{TRUE}.
 #' @return a list with the concentration index estimates requested (and standard errors, where applicable)
 #' @export
 
-mochi <- function(outcome, unval_exposure, val_exposure, return_naive = FALSE, include_se = FALSE, rank_ascend = TRUE) {
+mochi <- function(outcome, unval_exposure, val_exposure, return_naive = FALSE, include_se = FALSE, bootstraps = 1000, conf_level = 0.95, rank_ascend = TRUE) {
+  # If requested, negate exposures to make larger values more disadvantaged (lower rank)
   if(!rank_ascend) {
-    unval_exposure = unval_exposure * -1
-    val_exposure = val_exposure * -1
+    unval_exposure <- unval_exposure * -1
+    val_exposure <- val_exposure * -1
   }
+  
   # Estimate concentration index 
   c_hat <- mochi_estimate(outcome = outcome, 
                           unval_exposure = unval_exposure, 
@@ -23,25 +27,54 @@ mochi <- function(outcome, unval_exposure, val_exposure, return_naive = FALSE, i
   
   # If requested, calculate jacknife standard error
   if(include_se) {
-    n <- length(outcome) ## total number of observations
-    jack_ci_mb <- rep(NA, n) ## initialize empty vector
-    for(i in 1:n) { ## loop over sample, leaving out each row one-by-one
-      ## Define vectors excluding row i 
-      Y <- outcome[-i] 
-      Xstar <- unval_exposure[-i]
-      X <- val_exposure[-i]
-      
-      ## Compute moment-based concentration index excluding row i
-      jack_ci_mb[i] <- mochi_estimate(
-        outcome = Y, 
-        unval_exposure = Xstar,
-        val_exposure = X,
-        return_naive = FALSE)[[1]]
+    if (bootstraps == 0) {
+      n <- length(outcome) ## total number of observations
+      jack_ci_mb <- rep(NA, n) ## initialize empty vector
+      for(i in 1:n) { ## loop over sample, leaving out each row one-by-one
+        ## Define vectors excluding row i 
+        Y <- outcome[-i] 
+        Xstar <- unval_exposure[-i]
+        X <- val_exposure[-i]
+        
+        ## Compute moment-based concentration index excluding row i
+        jack_ci_mb[i] <- mochi_estimate(
+          outcome = Y, 
+          unval_exposure = Xstar,
+          val_exposure = X,
+          return_naive = FALSE)[[1]]
+      }
+      ## Calculate standard error from vector using jacknife formula
+      se <- sqrt((n - 1) / n * sum((jack_ci_mb - mean(jack_ci_mb)) ^ 2))
+      lb <- quantile(x = jack_ci_mb, probs = (1 - conf_level) / 2)
+      ub <- quantile(x = jack_ci_mb, probs = (1 - (1 - conf_level) / 2))
+    } else {
+      n <- length(outcome) ## total number of observations
+      nv <- sum(!is.na(val_exposure)) ## number of validated observations
+      which_nv <- which(!is.na(val_exposure)) ## indices of validated observations
+      boot_ci_mb <- rep(NA, bootstraps) ## initialize empty vector
+      for(i in 1:bootstraps) { ## loop over sample, resampling n each time
+        ## Resample indices (stratified by validation)
+        id_nv <- sample(x = which_nv, size = nv, replace = TRUE)
+        id <- sample(x = setdiff(1:n, which_nv), size = (n - nv), replace = TRUE)
+        ## Define vectors excluding row i 
+        Y <- outcome[c(id_nv, id)] 
+        Xstar <- unval_exposure[c(id_nv, id)]
+        X <- val_exposure[c(id_nv, id)]
+        
+        ## Compute moment-based concentration index excluding row i
+        boot_ci_mb[i] <- mochi_estimate(
+          outcome = Y, 
+          unval_exposure = Xstar,
+          val_exposure = X,
+          return_naive = FALSE)[[1]]
+      }
+      ## Calculate standard error and central conf_level x 100% interval from vector
+      se <- sd(boot_ci_mb)
+      lb <- quantile(x = boot_ci_mb, probs = (1 - conf_level) / 2)
+      ub <- quantile(x = boot_ci_mb, probs = (1 - (1 - conf_level) / 2))
     }
-    ## Calculate standard error from vector using jacknife formula
-    se_jack <- sqrt((n - 1) / n * sum((jack_ci_mb - mean(jack_ci_mb)) ^ 2))
   } else {
-    se_jack <- NA
+    se <- lb <- ub <- NA
   }
   
   # Return estimates
@@ -49,7 +82,8 @@ mochi <- function(outcome, unval_exposure, val_exposure, return_naive = FALSE, i
     return(
       list(
         ci_moment = c_hat[["ci_moment"]],
-        se_ci_moment = se_jack,
+        se_ci_moment = se,
+        confint_ci_moment = c(lb, ub), 
         ci_naive = c_hat[["ci_naive"]], 
         se_ci_naive = c_hat[["se_ci_naive"]]
         )
@@ -58,7 +92,8 @@ mochi <- function(outcome, unval_exposure, val_exposure, return_naive = FALSE, i
     return(
       list(
         ci_moment = c_hat[["ci_moment"]],
-        se_ci_moment = se_jack
+        se_ci_moment = se, 
+        confint_ci_moment = c(lb, ub)
         )
     )
   }
