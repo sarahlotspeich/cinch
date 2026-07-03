47da8bd0e9b657c9fe098884ff1faf6f76a1d8bb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-07-03 08:03:48 -0400

    Create helper file to count reps per sim setting

---
+++ simulations/count_reps_per_setting.R
@@ -0,0 +1,34 @@
+# Read in simulation results
+res = read.csv("~/Documents/mochi/simulations/vary_error_variance.csv") |> 
+  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
+
+# Check that 1000 replications ran per setting 
+table(res$true_ci, res$error_var) ## Yes! 
+
+# Read in simulation results
+res = read.csv("~/Documents/mochi/simulations/diff_error.csv") |> 
+  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
+
+# Check that 1000 replications ran per setting 
+table(res$true_ci, res$diff_mult) ## Yes! 
+
+# Read in simulation results
+res = read.csv("~/Documents/mochi/simulations/vary_validation_size.csv") |> 
+  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
+
+# Check that 1000 replications ran per setting 
+table(res$true_ci, res$val_prop) ## Yes! 
+
+# Read in simulation results
+res = read.csv("~/Documents/mochi/simulations/vary_total_size.csv") |> 
+  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
+
+# Check that 1000 replications ran per setting 
+table(res$true_ci, res$total_size) ## Yes! 
+
+# Read in simulation results
+res = read.csv("~/Documents/mochi/simulations/try_bootstrap.csv") |> 
+  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
+
+# Check that 1000 replications ran per setting 
+table(res$true_ci, res$total_size) ## Yes! 
