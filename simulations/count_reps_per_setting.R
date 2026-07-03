295d2ec1c1450d082eeab34e6af3ebcfb42b9bc3
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-07-03 08:08:18 -0400

    Update

--- simulations/count_reps_per_setting.R
+++ simulations/count_reps_per_setting.R
@@ -20,9 +20,7 @@ res = read.csv("~/Documents/mochi/simulations/vary_validation_size.csv") |>
 table(res$true_ci, res$val_prop) ## Yes! 
 
 # Read in simulation results
-res = read.csv("~/Documents/mochi/simulations/vary_total_size.csv") |> 
-  dplyr::filter(!is.na(true_ci)) ## exclude one all-NA row (didn't run)
-
+res = read.csv("~/Documents/mochi/simulations/vary_total_size.csv")
 # Check that 1000 replications ran per setting 
 table(res$true_ci, res$total_size) ## Yes! 
 
